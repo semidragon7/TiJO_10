3bb0f0599885f098f31983b936b0cbaf154b3404
MichalRodak <semidragon@interia.pl>
2016-05-11 10:16:33 +0200

    added

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -1,25 +1,33 @@
 describe('app', function () {
     'use strict';
 
-    var app = window.app;
+var app = window.app;
 
-    describe('assignToSwimmingCourse', function () {
-        it('should assign adult person to adult group', function () {
-            expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
-                name: 'Jack', age: 20, course: 'adults'
-            });
-        });
-        it('should assign person between the ages of 12 - 17 to teens group', function () {
-            expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
-                name: 'John', age: 14, course: 'teens'
-            });
-        });
-        it('should assign person below 12 to kids group', function () {
-            expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
-                name: 'Zack', age: 5, course: 'kids'
-            });
+describe('assignToSwimmingCourse', function () {
+    it('should assign adult person to adult group', function () {
+        expect(app.assignToSwimmingCourse('Jack', '10/10/1995')).toEqual({
+            name: 'Jack', age: 20, course: 'adults'
         });
     });
+    it('should assign person between the ages of 12 - 17 to teens group', function () {
+        expect(app.assignToSwimmingCourse('John', '07/10/2001')).toEqual({
+            name: 'John', age: 14, course: 'teens'
+        });
+    });
+    it('should assign person below 12 to kids group', function () {
+        expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
+            name: 'Zack', age: 5, course: 'kids'
+        });
+    });
+
+    it('should assign person below 12 to kids group', function () {
+        expect(app.assignToSwimmingCourse('Zack', '10/06/2010')).toEqual({
+            name: 'sad', age: 5, course: 'ss'
+        });
+    });
+
+
+});
 });
 
 
